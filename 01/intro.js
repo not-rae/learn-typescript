c5493886233fd75554bc517cb416058997e2d7b6
Jullene Rae Arsenal <jsarsenal@up.edu.ph>
2024-12-02 19:31:01 +0800

    initial commit

--- 01/intro.js
+++
@@ -1,3 +0,0 @@
-var user = { name: "arsenal", age: 21 };
-console.log("Arsenal");
-console.log(user.email);
--- 01/intro.ts
+++
@@ -1,4 +0,0 @@
-const user = {name: "arsenal", age: 21}
-
-console.log("Arsenal");
-console.log(user.email);
